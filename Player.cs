60bc5987ef24e289a5206fe0ed0ac83994c7171b
MrAzarel <93323890+MrAzarel@users.noreply.github.com>
2022-10-06 18:51:44 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WTF
+{
+    class Player
+    {
+        public int playerX = 2;
+        public int playerY = Field.fieldWidth - 1;
+        public char playerTile = 'O';
+
+        public bool isGrounded = true;
+
+        public void Jump()
+        {           
+            for (int i = 0; i < 4; i++)
+            {
+                Thread.Sleep(50);
+                Console.SetCursorPosition(playerX, playerY);
+                Console.WriteLine(" ");
+                if (i <= 1)
+                {
+                    playerY--;
+                    playerX++;
+                }
+                else
+                {
+                    playerY++;
+                    playerX++;
+                }
+                Console.SetCursorPosition(playerX, playerY);
+                Console.WriteLine(playerTile);
+            }
+            isGrounded = true;
+        }
+
+        public bool GroundCheck()
+        {
+            if (playerX == 6 || playerX == 23 || playerX == 55 || playerX == 70)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public bool Win()
+        {
+            if (playerX == 96)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace WTF
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Field field = new Field();
+            field.CreateField();
+            Player player = new Player();
+            while (true)
+            {
+                Console.SetCursorPosition(player.playerX, player.playerY);
+                Console.WriteLine(player.playerTile);
+                Thread.Sleep(10);
+
+                
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.W:
+                        if (player.playerX <= Field.fieldLength - 5)
+                        {
+                            player.isGrounded = false;
+                            while (!player.isGrounded)
+                            {
+                                player.Jump();
+                            }
+                        }
+                            break;
+                    case ConsoleKey.A:
+                        if (player.playerX != 1)
+                        {
+                            Console.SetCursorPosition(player.playerX, player.playerY);
+                            Console.WriteLine(" ");
+                            player.playerX--;
+                        }
+                        break;
+
+                    case ConsoleKey.D:
+                        if (player.playerX != Field.fieldLength-1)
+                        {
+                            Console.SetCursorPosition(player.playerX, player.playerY);
+                            Console.WriteLine(" ");
+                            player.playerX++;
+                        }
+                        break;
+                }
+                if (player.GroundCheck())
+                {
+                    Console.SetCursorPosition(Field.fieldLength / 2, Field.fieldWidth / 2);
+                    Console.WriteLine("Вы проиграли");
+                    break;
+                }
+                if (player.Win())
+                {
+                    Console.SetCursorPosition(Field.fieldLength / 2, Field.fieldWidth / 2);
+                    Console.WriteLine("Вы выиграли!");
+                    break;
+                }
+            }
+        }
+    }
+}
